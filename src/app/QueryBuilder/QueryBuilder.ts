4d07a7c08d140033ee61537a528027067e5aba04
Md.Samsul Arefin <samsularefin532@gmail.com>
2024-06-05 00:58:07 +0600

    CODE REFACTORING - search method DONE

---
+++ src/app/QueryBuilder/QueryBuilder.ts
@@ -0,0 +1,33 @@
+/* eslint-disable no-undef */
+import { FilterQuery, Query } from 'mongoose';
+
+class QueryBuilder<T> {
+  // modelQuery  =  all types of models;
+  public modelQuery: Query<T[], T>;
+  public query: Record<string, unknown>;
+  constructor(modelQuery: Query<T[], T>, query: Record<string, unknown>) {
+    this.modelQuery = modelQuery;
+    this.query = query;
+  }
+  search(searchFields: string[]) {
+    if (this?.query?.searchTerm) {
+      this.modelQuery = this.modelQuery.find({
+        $or: searchFields.map(
+          (field) =>
+            ({
+              [field]: { $regex: searchTerm, $options: 'i' },
+            }) as FilterQuery<T>,
+        ),
+      });
+    }
+    return this;
+  }
+  filter() {
+    const queryObj = { ...this.query };
+    //filtering
+    const excluding = ['searchTerm', 'sort', 'limit', 'page', 'fields'];
+    excluding.forEach((val) => delete queryObj[val]);
+  }
+}
+
+export default QueryBuilder;
