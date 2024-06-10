6a0aa49473e9b7adb82743088f327e6c2b6e6e05
Md.Samsul Arefin <samsularefin532@gmail.com>
2024-06-10 20:55:05 +0600

    user authantication checking

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,6 +1,28 @@
+import httpStatus from 'http-status';
+import AppError from '../../errrors/appError';
+import { userModel } from '../user/user.model';
 import { TSignUser } from './auth.interface';
+import bcrypt from 'bcrypt';
+const loginUser = async (payload: TSignUser) => {
+  const isUserExists = await userModel.findOne({ id: payload.id });
 
-const loginUser = async (payload: TSignUser) => {};
+  if (!isUserExists) {
+    throw new AppError(httpStatus.NOT_FOUND, 'This user does not exists!');
+  }
+  // check if the user is deleted
+  if (isUserExists?.isDeleted) {
+    throw new AppError(httpStatus.FORBIDDEN, 'This user is already deleted!');
+  }
+  // check the user given password;
+  const isPasswordMatched = await bcrypt.compare(
+    payload.password,
+    isUserExists?.password,
+  );
+  // check if the user is blocked
+  if (isUserExists?.status === 'blocked') {
+    throw new AppError(httpStatus.FORBIDDEN, 'This user is blocked!');
+  }
+};
 
 export const authService = {
   loginUser,
